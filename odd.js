9fd799e644881b92428e17dded6e0fb0eb077be9
shawon <shawon.softeng@gmail.com>
2023-08-28 01:30:28 +0600

    commit for recursion

--- odd.js
+++ odd.js
@@ -11,3 +11,21 @@ const findOddNumber = (arr) => {
 console.log(findOddNumber([3, 6, 8, 23, 56, 87, 54, 33, 79, 45, 67]));
 
 // with helper method
+
+const findOddNumber2 = (arr) => {
+    let newArr = [];
+
+    function helper(input) {
+        if (input === 0) return 0;
+
+        if (input[0] % 2 !== 0) {
+            newArr.push(input[0]);
+        }
+        helper(input.slice(1));
+    }
+
+    helper(arr);
+    return newArr;
+};
+
+console.log(findOddNumber2([3, 6, 8, 23, 56, 87, 54, 33, 79, 45, 67]));
